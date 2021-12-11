2ff1c14d5c3d40f5f2b8111e1c06c32aa7539acf
DESKTOP-6LPJTTU\olik3 <olik369@gmail.com>
2021-12-12 01:29:40 +0900

    111

--- 씹어먹는 C++/chp04-05.내가 만드는 String 클래스/01.직접 만드는 문자열 클래스(MyString)/main.cpp
+++ 씹어먹는 C++/chp04-05.내가 만드는 String 클래스/01.직접 만드는 문자열 클래스(MyString)/main.cpp
@@ -1,5 +1,8 @@
-#include "selfString.h"
+#include "MyString.h"
 
 int main() {
-	auto a = "self";
+    MyString str1("abcdef");
+    MyString str2("abcde");
+
+    std::cout << "str1 and str2 compare : " << str1.compare(str2) << std::endl;
 }
